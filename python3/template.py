34b22c1bc5d328e2ee1e6b7d3680b6a905a42d4e
0x65-e <16930669+0x65-e@users.noreply.github.com>
2022-12-11 12:06:38 -0800

    Extend template to Part 2

--- python3/template.py
+++ python3/template.py
@@ -1,8 +1,14 @@
 
-def solve00(input: list[str]) -> None:
+def solve00_p1(input: list[str]) -> None:
+    pass
+
+def solve00_p2(input: list[str]) -> None:
     pass
 
 if __name__ == "__main__":
     with open("../inputs/input00.txt") as input_file:
         input = input_file.read().splitlines() # Strip trailing newlines
-    solve00(input)
+    print("Part 1 solution:")
+    solve00_p1(input)
+    print("\nPart 2 solution:")
+    solve00_p2(input)
